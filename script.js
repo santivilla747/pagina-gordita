c71f63d037fce3edc7e2a104000a2a0a2fd20e11
santivilla747 <54n71v11ll4@gmail.com>
2026-09-17 17:27:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const boton = document.querySelector(".boton-sorpresa");
+const mensaje = document.querySelector("#mensaje-sorpresa");
+
+boton.addEventListener("click", function () {
+    mensaje.style.display = "block";
+});
